bc1b19d022d34fbc648adbf34bc1cefd54f69de5
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-24 12:13:29 -0300

    feature: implement document initial props

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,50 @@
-import Document, { Html, Head, Main, NextScript } from "next/document"
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument  extends Document{
-  render(){
-    return(
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render(): JSX.Element {
+    return (
       <Html>
         <Head>
           <link rel="shortcut icon" href="/favicon.png" type="image/png" /> 
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
