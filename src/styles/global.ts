d50cfbba0df5a9727d72e1eb913f57aa1c9809e6
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-24 14:43:12 -0300

    style: add global style settings

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,26 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+
+    font-family: 'Poppins', sans-serif;
+  }
+
+  body {
+    position: relative;
+
+    width: 100vw;
+    height: 100vh;
+
+    background: ${props => props.theme.colors.mintCream[500]};
+  }
+
+  button {
+    cursor:pointer;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
   }
 `
