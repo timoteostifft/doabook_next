8625be975e136efbf26f2c235d386832d6deceee
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-24 14:44:15 -0300

    style: create login form component

---
+++ src/components/LoginForm/styles.ts
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 768px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 16px;
+
+  img {
+    width: 305px;
+    height: auto;
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 305px;
+
+  form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    input {
+      border: none;
+      max-width: 320px;
+      width: 100%;
+      height: 32px;
+    }
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+
+    button {
+      width: 140px;
+      height: 54px;
+      font-weight: 500;
+      border: none;
+      color: ${props => props.theme.colors.mintCream[500]};
+
+      &:nth-child(1){
+        background: ${props => props.theme.colors.mango[500]}
+      }
+
+      &:nth-child(2){
+        background: ${props => props.theme.colors.brunswickGreen[300]}
+      }
+    }
+  }
+`
