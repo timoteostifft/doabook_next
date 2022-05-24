7497330f3a12b8a746eaf9cb391dfee626b02e47
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-24 19:20:40 -0300

    feature: create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Content } from './styles'
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <h3>doaBook</h3>
+      </Content>
+    </Container>
+  )
+}
+
+export default Header
