9c6c3500e25cfb9f1a5506ef8de4dfd803c32d6b
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-25 01:46:50 -0300

    style: create home page style

---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+  position: absolute;
+  z-index: 0;
+
+  width: 100%;
+  height: 100%;
+
+  div:nth-child(2) {
+      margin-top: 22.5px;
+  }
+  
+  div:nth-child(3) {
+      margin-top: 32.5px;
+  }
+
+  div:nth-child(4) {
+    margin-top: 28.5px;
+  }
+`
