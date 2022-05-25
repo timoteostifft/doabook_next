2758868dc7ae570a3db9c2b6c3bf86d879e29a06
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-25 15:17:34 -0300

    feature: add conditional rendering method to books list

--- src/components/BooksSection/index.tsx
+++ src/components/BooksSection/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useInstitutionContext } from '../../hooks/useInstitutionContext';
 import { Book } from '../../types/Book';
 import BookCard from '../BookCard';
 
@@ -9,11 +10,12 @@ interface BooksSectionProps {
 }
 
 const BooksSection: React.FC<BooksSectionProps> = ({ books }) => {
+  const { institution } = useInstitutionContext()
   return (
     <Container>
-      {books.map(book => (
-          <BookCard key={book.id} book={book} />
-        ))}
+      {books.map(book => (book.institution_id === institution.id) && (
+        <BookCard key={book.id} book={book} />
+      ))}
     </ Container>
   );
 }
