ba3f0bc17e16dc5d64699fae5ac4dd73cd62f86b
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-25 01:47:23 -0300

    style: add colors to main theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,18 +1,21 @@
 const theme = {
   colors: {
+    englishVermilion: {
+      300: "#DB575B",
+      500: "#D64045"
+    },
     mintCream: {
       300: "#FDFFFD",
-      500: "#F2F7F2"
+      500: "#F2F7F2",
+      700: "#D9E8D9"
     },
     brunswickGreen: {
       300: "#5C9970",
       500: "#32533D",
       700: "#1F3325"
     },
-    zomp: {
-      500: "#32A287"
-    },
     mango: {
+      400: "#FAC64C",
       500: "#FABC2A"
     }
   }
