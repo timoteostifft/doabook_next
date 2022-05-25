2dd0047f5f75766ce5afee1eb7865c96cb591690
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-25 15:18:17 -0300

    feature: create instituions section page

--- src/components/InstitutionsSection/index.tsx
+++ src/components/InstitutionsSection/index.tsx
@@ -1,23 +1,35 @@
 import React, { useState } from 'react';
+import InstitutionsModal from '../InstitutionsModal';
+
 import { Institution } from '../../types/Institution';
+import { Container, Content } from './styles';
+
 import { VscListSelection as Select } from 'react-icons/vsc'
 
-import { Container, Content } from './styles';
+import { useInstitutionContext } from '../../hooks/useInstitutionContext'
 
 interface InstitutionsSectionProps {
   institutions: Institution[]
 }
 
 const InstitutionsSection: React.FC<InstitutionsSectionProps> = ({ institutions }) => {
-  const [ selectedInstitution, setSelectedInstitution ] = useState(institutions[0])
+
+  const { institution, setInstitution } = useInstitutionContext()
+
+  const [ isModalOpen, setIsModalOPen ] = useState<boolean>(false)
+
   return (
     <Container>
       <Content>
-        <h3>Instituição: {selectedInstitution.name}</h3>
-        <button>
+        <h3>Instituição: {institution.name}</h3>
+        <button onClick={() => setIsModalOPen(!isModalOpen)}>
           <Select />
         </button>
       </Content>
+
+      {isModalOpen && (
+        <InstitutionsModal institution={institution} institutions={institutions} />
+      )}
     </Container>
     )
 }
