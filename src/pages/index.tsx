ab9f7a95b9c6e420e1612b1533b186df3f12b9c1
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-25 01:48:29 -0300

    feature: add books to static page props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
+import InstitutionsSection from '../components/InstitutionsSection'
+import BooksSection from '../components/BooksSection'
 
 import Header from '../components/Header'
 import SearchBar from '../components/SearchBar'
 import api from '../services/api'
 import { Wrapper } from '../styles/pages/Home'
 import { Institution } from '../types/Institution'
+import { Book } from '../types/Book'
 
 interface HomeProps {
   institutions: Institution[]
+  books: Book[]
 }
 
-const Home: NextPage<HomeProps> = ({ institutions }) => {
+const Home: NextPage<HomeProps> = ({ institutions, books }) => {
   return (
     <Wrapper>
       <Head>
@@ -19,7 +23,9 @@ const Home: NextPage<HomeProps> = ({ institutions }) => {
       </Head>
 
       <Header />
+      <InstitutionsSection />
       <SearchBar />
+      <BooksSection books={books}/>
 
     </Wrapper>
   )
@@ -27,10 +33,12 @@ const Home: NextPage<HomeProps> = ({ institutions }) => {
 
 export const getStaticProps: GetStaticProps = async () => {
   const institutionsResponse = await api.get('/institutions')
+  const booksResponse = await api.get('/books')
 
   return {
     props: {
       institution: institutionsResponse.data,
+      books: booksResponse.data
     },
   }
 }
