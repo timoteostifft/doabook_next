a65a60d650d7c0d290300a6d7c87db9ba9b1c80e
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-25 15:14:04 -0300

    feature: set instituion provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import { InstitutionProvider } from '../contexts/Institution'
+
 
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <InstitutionProvider>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </InstitutionProvider>
   )
 }
 
