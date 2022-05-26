daa38414b88c010bf90d4808c1de522324de267d
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-26 14:05:21 -0300

    feature: implement must be logged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import InstitutionsSection from '../components/InstitutionsSection'
 import BooksSection from '../components/BooksSection'
+import { useRouter } from 'next/router'
 
 import Header from '../components/Header'
 import SearchBar from '../components/SearchBar'
@@ -9,6 +11,8 @@ import api from '../services/api'
 import { Wrapper } from '../styles/pages/Home'
 import { Institution } from '../types/Institution'
 import { Book } from '../types/Book'
+import { useAuthenticationContext } from '../hooks/useAuthenticationContext'
+import { useEffect } from 'react'
 
 interface HomeProps {
   institutions: Institution[]
@@ -16,6 +20,16 @@ interface HomeProps {
 }
 
 const Home: NextPage<HomeProps> = ({ institutions, books }) => {
+  const { authentication } = useAuthenticationContext()
+  const router = useRouter()
+  
+  if (!authentication?.token) {
+    useEffect(() => {
+      router.push("/login")
+      alert("Usuário precisa estar logado.")
+    })
+  }
+
   return (
     <Wrapper>
       <Head>
@@ -32,7 +46,7 @@ const Home: NextPage<HomeProps> = ({ institutions, books }) => {
 
 export const getStaticProps: GetStaticProps = async () => {
   const institutionsResponse = await api.get('/institutions')
-  const booksResponse = await api.get('/books')
+  const booksResponse = await api.get('/books/97c9ea6b-7bf6-424e-bafa-6b8213503080')
 
   return {
     props: {
