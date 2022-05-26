a63a319176c5e6fb4fc2bb4a9e7727fd056193ad
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-26 13:54:08 -0300

    style: update buttons icons

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -2,36 +2,42 @@ import React from 'react';
 import { Book } from '../../types/Book';
 import { Container, Content} from './styles';
 import dayjs from 'dayjs'
-import { AiFillEdit as Edit} from 'react-icons/ai'
-import { AiFillDelete as Delete } from 'react-icons/ai'
+import { BsFillBookmarkPlusFill as Select} from 'react-icons/bs'
+import { AiFillEdit as Edit } from 'react-icons/ai'
 
 
 interface BookCardProps {
   book: Book
+  isAdmin: Boolean
 }
 
-const BookCard: React.FC<BookCardProps> = ({ book }) => {
+const BookCard: React.FC<BookCardProps> = ({ book, isAdmin }) => {
+  function handleGetBook(id: string) {}
+
   return (
     <Container>
       <Content>
-      <section>
-        <h3>{book.name}</h3>
-        <p>· {book.edition}º Edição | {book.year}</p>
-        <p>· Condição: {book.condition}</p>
-        <p>· {book.quantity} disponíveis</p>
-        <p>· Lançamento: {dayjs(book.launch).format('DD/MM/YYYY')}</p>
+        <section>
+          <h3>{book.name}</h3>
+          <p>· {book.edition}º Edição | {book.year}</p>
+          <p>· Condição: {book.condition}</p>
+          <p>· {book.quantity} disponíveis</p>
+          <p>· Lançamento: {dayjs(book.launch).format('DD/MM/YYYY')}</p>
 
-      </section>
-      <nav>
-        <button>
-          <Edit />
-          Editar
-        </button>
-        <button>
-          <Delete />
-          Remover
-        </button>
-      </nav>
+        </section>
+        <nav>
+          <button>
+            <Select />
+            Levar
+          </button>
+          <button 
+            type="button" 
+            disabled={!isAdmin}
+            onClick={() => {console.log('Implementar edição')}}>
+            <Edit />
+            Editar
+          </button>
+        </nav>
       </ Content>
     </ Container>
   )
