5993aefe6b18cee1cf093c5149987387d8e3cb07
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-26 14:05:38 -0300

    style: create global style for modal overlay

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -28,4 +28,16 @@ export default createGlobalStyle`
       filter: brightness(0.9);
     }
   }
+
+  .react-modal-overlay{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+   
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `
