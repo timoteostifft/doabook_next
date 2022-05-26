6fdb7f9f90a76434b7b544adab11ccbba83a51df
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-26 14:03:07 -0300

    feature: create login method on login page using authentication context

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,18 +1,41 @@
 /* eslint-disable @next/next/no-img-element */
+import { useState } from 'react'
+import { useAuthenticationContext } from '../../hooks/useAuthenticationContext'
 import { Container, Content } from './styles'
 
 const LoginForm: React.FC = () => {
+  const { login } = useAuthenticationContext()
+
+  function handleLogin (email: string, password: string){
+    login({email, password})
+  }
+
+  const [ email, setEmail ] = useState<string>('')
+  const [ password, setPassword ] = useState<string>('')
+
   return (
     <Container>
       <img src="images/brand.png" alt="" />
       <Content>
       <form>
-        <input type="text" placeholder=' Email'/>
-        <input type="text" placeholder=' Senha'/>
+        <input 
+          type="text" 
+          placeholder='Email' 
+          required
+          onChange={event => setEmail(event.target.value)}
+        />
+        <input 
+          type="password" 
+          placeholder='Senha' 
+          required
+          onChange={event => setPassword(event.target.value)}
+        />
       </form> 
       <nav>
         <button>Cadastro</button>
-        <button>Login</button>
+        <button onClick={() => handleLogin(email, password)}>
+          Login
+        </button>
       </nav>
       </Content>
     </Container>
